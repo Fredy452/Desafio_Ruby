55234142657b9a8d5d70bbf1c1f27c0e6e4235f3
Fredy Arce <fredyarce452@gmail.com>
2022-10-29 13:32:07 -0300

    Cambio a postgresql

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :navbars
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'home', to: 'home#inicio'
+  get '/', to: 'home#inicio'
   # Defines the root path route ("/")
   # root "articles#index"
 end
